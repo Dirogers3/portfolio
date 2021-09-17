12db69afd7a882885096ee5751a8a1c5b591786d
dirogers <rog17006@byui.edu>
2021-09-16 18:50:10 -0700

    Created basic structure for the portfolio

--- js/main.js
+++ js/main.js
@@ -2,5 +2,13 @@ const links = [
     {
         label: "Week 1 notes",
         url : "week1/index.html"
+    },
+    {
+        label: "Week 2 notes",
+        url : "week2/index.html"
     }
 ]
+// get pointer to <ul> element
+var list = document.querySelector('div');
+links.forEach(link => {list.innerHTML+= `<li><a href="${link.url}">${link.label}</a></li>`;});
+
