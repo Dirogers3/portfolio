7ec3e47d8fef8291b4a56769978732d5b1f0fe55
dirogers <rog17006@byui.edu>
2021-11-20 17:45:59 -0800

    Finished the Notes and project for week 10

--- week10/script.js
+++ week10/script.js
@@ -1,11 +1,35 @@
-//fetch from the api
-function getPokemon(){
- fetch('https://pokeapi.co/api/v2/pokemon?limit=10').then(function(response){
-   return response.json();
- })
-}
-fetchedData = getPokemon();
-document.getElementById("pokemon-list").innerHTML = 
-`
-<li>${fetchedData}</li>
-`;
+//get pokemon name from form and fetch from api
+
+function getPokemonByName(){
+    var pokemonName = document.getElementById("pokemonName").value;
+    console.log(pokemonName);
+    pokemonNames = pokemonName.toLowerCase();
+    fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonNames}`)
+    .then(response => response.json())
+    .then(data => {
+        var abilities = data.abilities;
+        document.getElementById("pokemon-list").innerHTML = `
+
+        <div class="pokemon-card">
+        <h1 id="pokemon-name" style="text-transform:capitalize;">${data.name}</h1>
+        <h3 style="text-transform:capitalize;">Type: ${data.types[0].type.name}</h3>
+        <h3>Height: ${data.height} lb</h3>
+        <h3>Weight: ${data.weight} ft</h3>
+         <h3>Abilities:</h3>
+        <ul>
+        ${abilities.map(ability => `<li>${ability.ability.name}</li>`).join('')}
+        </ul>
+        </div>
+        `;
+    })
+    .catch(error => {
+        document.getElementById("pokemon-list").innerHTML = `
+        <div class="error">
+        <h3 class="error">No Pokemon Found</h3>
+        </div>
+        `;
+        document.getElementById('pokemonName').value = "";
+
+    });
+}   
+
