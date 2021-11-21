e30d75e53580df4897dfef70a4ff16b235a7f4a5
dirogers <rog17006@byui.edu>
2021-11-20 17:47:02 -0800

    update

--- week10/script.js
+++ week10/script.js
@@ -13,8 +13,8 @@ function getPokemonByName(){
         <div class="pokemon-card">
         <h1 id="pokemon-name" style="text-transform:capitalize;">${data.name}</h1>
         <h3 style="text-transform:capitalize;">Type: ${data.types[0].type.name}</h3>
-        <h3>Height: ${data.height} lb</h3>
-        <h3>Weight: ${data.weight} ft</h3>
+        <h3>Height: ${data.height} ft</h3>
+        <h3>Weight: ${data.weight} lb</h3>
          <h3>Abilities:</h3>
         <ul>
         ${abilities.map(ability => `<li>${ability.ability.name}</li>`).join('')}
